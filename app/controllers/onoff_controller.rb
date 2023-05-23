9902e234514e20519bb7c1cf0a1893a15132fe46
miloneko <satohannn@gmail.com>
2023-05-23 10:12:57 +0900

    Fix:構文修正

--- app/controllers/onoff_controller.rb
+++ app/controllers/onoff_controller.rb
@@ -1,11 +1,11 @@
 class OnoffController < ApplicationController
    def index
-        render json: on_off
+     render json: on_off
     end
 
     def beep_on
-        system('python /home/pi/ruby/pi/led-api/python/beep_on.py')
-        render json: { message: 'Beep on!' }
+      system('python /home/pi/ruby/pi/led-api/python/beep_on.py')
+      render json: { message: 'Beep on!' }
     end
 
     def beep_off
@@ -14,42 +14,42 @@ class OnoffController < ApplicationController
     end
 
     def blue_on
-    system('python /home/pi/ruby/pi/led-api/python/blue_on.py')
-    render json: { message: 'Blue on! '}
+      system('python /home/pi/ruby/pi/led-api/python/blue_on.py')
+      render json: { message: 'Blue on! '}
     end
 
     def blue_off
-    system('python /home/pi/ruby/pi/led-api/python/blue_off.py')
-    render json: { message: 'Blue off! '}
+      system('python /home/pi/ruby/pi/led-api/python/blue_off.py')
+      render json: { message: 'Blue off! '}
     end
 
     def green_on
-    system('python /home/pi/ruby/pi/led-api/python/green_on.py')
-    render json: { message: 'Green on! '}
+      system('python /home/pi/ruby/pi/led-api/python/green_on.py')
+      render json: { message: 'Green on! '}
     end
 
     def green_off
-    system('python /home/pi/ruby/pi/led-api/python/green_off.py')
-    render json: { message: 'Green off! '}
+      system('python /home/pi/ruby/pi/led-api/python/green_off.py')
+      render json: { message: 'Green off! '}
     end
 
     def red_on
-    system('python /home/pi/ruby/pi/led-api/python/red_on.py')
-    render json: { message: 'Red on! '}
+      system('python /home/pi/ruby/pi/led-api/python/red_on.py')
+      render json: { message: 'Red on! '}
     end
 
     def red_off
-    system('python /home/pi/ruby/pi/led-api/python/red_off.py')
-    render json: { message: 'Red off! '}
+      system('python /home/pi/ruby/pi/led-api/python/red_off.py')
+      render json: { message: 'Red off! '}
     end
 
     def white_on
-    system('python /home/pi/ruby/pi/led-api/python/white_on.py')
-    render json: { message: 'White on! '}
+      system('python /home/pi/ruby/pi/led-api/python/white_on.py')
+      render json: { message: 'White on! '}
     end
 
     def white_off
-    system('python /home/pi/ruby/pi/led-api/python/white_off.py')
-    render json: { message: 'White off! '}
+      system('python /home/pi/ruby/pi/led-api/python/white_off.py')
+      render json: { message: 'White off! '}
     end
 end
